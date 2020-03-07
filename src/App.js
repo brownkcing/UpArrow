063911d4a68ed2fc3cc9a09d8b82559be0347410
Cyril <cyril.marcus@emirates.com>
2020-03-07 09:32:33 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles/styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import  Scroll  from './components/Scroll'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{height:"2000px"}}>
+          <Scroll showBelow={500} />
+          <h3>Scroll down</h3>
+          <h3><FontAwesomeIcon icon={faArrowDown} /></h3>
+        </div> 
       </header>
     </div>
   );
